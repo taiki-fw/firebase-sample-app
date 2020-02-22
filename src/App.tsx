c74c7dc77fc49b35263e8c60aebf004dd00b14f5
taiki-fw <taiki_52@outlook.jp>
2020-02-22 16:29:30 +0900

    feat:simple google auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import firebase from "./firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+interface UserStatus {
+  user: firebase.User | null;
+}
+
+class App extends React.Component {
+  public state: UserStatus = {
+    user: null
+  };
+
+  public componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user);
+      this.setState({ user });
+    });
+  }
+
+  public login() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+  }
+
+  public logout() {
+    firebase.auth().signOut();
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          名前: {this.state.user && this.state.user.displayName}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p className="App-intro">
+          UID: {this.state.user && this.state.user.uid}
+        </p>
+
+        {this.state.user ? (
+          <button onClick={this.logout}>Google Logout</button>
+        ) : (
+          <button onClick={this.login}>Google Login</button>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
