a329dcd59802095b9769844b52691f1e78b96616
taiki-fw <taiki_52@outlook.jp>
2020-02-23 00:06:26 +0900

    feat:loginユーザーのトークン表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
+import axios from "axios";
 import "./App.css";
 import firebase from "./firebase";
+import PlayList from "./PlayList/PlayList.js";
 
 interface UserStatus {
   user: firebase.User | null;
@@ -16,11 +18,43 @@ class App extends React.Component {
       console.log(user);
       this.setState({ user });
     });
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then(function(result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential;
+          console.log(token);
+        }
+        // The signed-in user info.
+        var user = result.user;
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
   }
 
   public login() {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithRedirect(provider);
+    // axios.get("https://accounts.google.com/o/oauth2/auth", {
+    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //   params: {
+    //     client_id:
+    //       "875303700757-fnch5dcl9cpjosm29qgsf4vt5g4dipsi.apps.googleusercontent.com",
+    //     redirect_uri: "http://localhost:3000",
+    //     response_type: "token",
+    //     scope: "https://www.googleapis.com/auth/youtube"
+    //   }
+    // });
   }
 
   public logout() {
@@ -42,6 +76,7 @@ class App extends React.Component {
         ) : (
           <button onClick={this.login}>Google Login</button>
         )}
+        <PlayList />
       </div>
     );
   }
