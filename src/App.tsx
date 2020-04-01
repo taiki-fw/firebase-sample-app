10e83faf709d92bd56429995a70d32d30c6d6bff
taiki-fw <taiki_52@outlook.jp>
2020-04-01 15:36:50 +0900

    feat:classからhooksに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,48 @@
-import * as React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import firebase from "./firebase";
 import Input from "./Components/Input";
 import Content from "./Components/Content";
 
-interface UserStatus {
-  user: firebase.User | null;
-}
+const App = () => {
+  const [user, setUser] = useState<firebase.User | null>(null);
 
-class App extends React.Component {
-  public state: UserStatus = {
-    user: null
-  };
-
-  public componentDidMount() {
+  useEffect(() => {
     firebase.auth().onAuthStateChanged(user => {
-      this.setState({ user });
+      setUser(user);
     });
-  }
+  }, [user]);
 
-  public login() {
+  const login = useCallback(() => {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithRedirect(provider);
-  }
+  }, []);
 
-  public logout() {
+  const logout = useCallback(() => {
     firebase.auth().signOut();
-  }
+  }, []);
 
-  public render() {
-    return (
-      <div className="App">
+  return (
+    <div className="App">
+      {user?.photoURL ? (
         <img
-          src={this.state.user?.photoURL || ""}
+          src={user.photoURL}
           alt="ログインユーザーのアイコン"
           width="200"
           height="200"
           style={{ borderRadius: "50%" }}
         />
-        <p className="App-intro">
-          {this.state.user && this.state.user.displayName}
-        </p>
-        {this.state.user ? (
-          <button onClick={this.logout}>Google Logout</button>
-        ) : (
-          <button onClick={this.login}>Google Login</button>
-        )}
-        <Input />
-        <Content />
-      </div>
-    );
-  }
-}
+      ) : null}
+      <p className="App-intro">{user && user.displayName}</p>
+      {user ? (
+        <button onClick={logout}>Google Logout</button>
+      ) : (
+        <button onClick={login}>Google Login</button>
+      )}
+      <Input />
+      <Content />
+    </div>
+  );
+};
 
 export default App;
